24615e8dcae487044589448da818d75eadb1f3e6
staaph <contact.staaph@gmail.com>
2022-04-06 23:38:36 +0200

    tweaked design

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -7,10 +7,11 @@
     >
       <!-- Songs Viewer for song in songs-->
       <div
-        class="bg-gray-900 flex flex-col sm:w-82 m-5 h-800 sm:h-440 rounded-xl"
+        class="bg-gray-900 flex flex-col sm:w-82 m-5 h-800 sm:h-440 rounded-xl overflow-y-auto"
       >
+        <!-- START SONG COMPONENT -->
         <div
-          class="flex flex-row bg-gray-700 m-5 p-2 rounded-lg active:scale-95 cursor-pointer"
+          class="flex flex-row bg-gray-700 mx-4 mt-4 p-2 rounded-lg active:scale-95 cursor-pointer transition ease-in-out delay-50"
         >
           <!-- Song Picture -->
           <div class="w-1/6">
@@ -25,16 +26,45 @@
             <p
               class="text-gray-300 text-xs items-center justify-center flex flex-col w-full"
             >
-              Bo Burnham
+              Artist
             </p>
             <p class="text-white">Song</p>
           </div>
-          <div class="w-1/6"></div>
+          <div class="w-1/6 flex items-center">
+            <p class="m-3 text-white text-sm">00:00</p>
+          </div>
         </div>
+        <!-- END SONG COMPONENT -->
+        <!-- START SONG COMPONENT -->
+        <div
+          class="flex flex-row bg-gray-700 mx-4 mt-4 p-2 rounded-lg active:scale-95 cursor-pointer transition ease-in-out delay-50"
+        >
+          <!-- Song Picture -->
+          <div class="w-1/6">
+            <div
+              class="flex flex-row bg-white m-3 p-6 w-16 justify-center rounded-xl"
+            >
+              <font-awesome-icon icon="music" />
+            </div>
+          </div>
+          <!-- Artist & Songname -->
+          <div class="w-full flex flex-col items-center justify-center">
+            <p
+              class="text-gray-300 text-xs items-center justify-center flex flex-col w-full"
+            >
+              Artist
+            </p>
+            <p class="text-white">Song</p>
+          </div>
+          <div class="w-1/6 flex items-center">
+            <p class="m-3 text-white text-sm">00:00</p>
+          </div>
+        </div>
+        <!-- END SONG COMPONENT -->
       </div>
       <!-- Play Bar -->
       <div
-        class="flex justify-between bg-gray-900 sm:w-82 h-16 rounded-xl mt-2 mx-5 mb-6 px-5"
+        class="flex justify-between bg-gray-900 sm:w-82 h-16 rounded-2xl mt-2 mx-5 mb-6 px-5"
       >
         <!-- Play Button -->
         <button type="button" class="">
@@ -50,11 +80,11 @@
           00:00
         </div>
         <!-- current position / change - bar + song title & artist -->
-        <div class="flex flex-col px-4 items-center w-full">
+        <div class="flex flex-col px- items-center w-full">
           <span class="pt-2 text-gray-400 text-sm pb-2"
             >Song title by artist</span
           >
-          <div class="w-full sm:w-3/4 bg-gray-200 rounded-xl">
+          <div class="w-3/4 bg-gray-200 rounded-xl">
             <div class="bg-blue-700 h-1 rounded-xl" style="width: 45%"></div>
           </div>
         </div>
