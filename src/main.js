d9bb98906b7fd06f46e4ce4608bac66b47b20e45
staaph <contact.staaph@gmail.com>
2022-04-06 15:34:46 +0200

    added songs property

--- src/main.js
+++ src/main.js
@@ -9,13 +9,13 @@ import './assets/index.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 /* import specific icons */
-import { faPlay, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faCircle, faMusic } from '@fortawesome/free-solid-svg-icons';
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 /* add icons to the library */
-library.add(faPlay, faCircle);
+library.add(faPlay, faCircle, faMusic);
 
 /* add font awesome icon component */
 
